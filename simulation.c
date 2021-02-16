a17f21aa68e997e8d64e4935511cfd60c7192cf6
Jaakko Julin <jaakko.julin@jyu.fi>
2021-02-16 16:28:38 +0200

    Copyright notices added.

--- simulation.c
+++ simulation.c
@@ -1,3 +1,16 @@
+/*
+
+    Jaakko's Backscattering Simulator (JaBS)
+    Copyright (C) 2021 Jaakko Julin
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    See LICENSE.txt for the full license.
+
+ */
 #include "simulation.h"
 
 sim_workspace *sim_workspace_init(const simulation *sim, sample *sample, jibal_gsto *gsto) {
