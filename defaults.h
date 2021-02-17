cb3cd29beb8cc7f9d057cdc4fabdff8e9de830c0
Jaakko Julin <jaakko.julin@jyu.fi>
2021-02-17 10:49:03 +0200

    Moved default values from simulation.c to defaults.h

---
+++ defaults.h
@@ -0,0 +1,16 @@
+#ifndef JABS_DEFAULTS_H
+#define JABS_DEFAULTS_H
+
+/* Defaults for new simulations */
+#define ENERGY (2.0*C_MEV)
+#define ALPHA (15.0*C_DEG)
+#define BETA (0.0*C_DEG)
+#define THETA (165.0*C_DEG)
+#define DETECTOR_RESOLUTION (15.0*C_KEV/C_FWHM)
+#define PARTICLES_SR (1.0e12)
+#define E_MIN (100.0*C_KEV)
+#define ENERGY_SLOPE (1.0*C_KEV)
+#define STOP_STEP_INCIDENT (5.0*C_KEV)
+#define STOP_STEP_EXITING (25.0*C_KEV)
+
+#endif // JABS_DEFAULTS_H
