7482b8b0aa488b2175d3f46e38bbcc6ec9836c5d
Jaakko Julin <jaakko.julin@jyu.fi>
2021-04-12 09:55:44 +0300

    Removed prototype from fit.c which is provided by a header file.

--- fit.c
+++ fit.c
@@ -21,8 +21,7 @@
 #include <gsl/gsl_blas.h>
 
 #include "fit.h"
-
-void no_ds(sim_workspace *ws, const sample *sample); /* TODO: move this function away from main so we don't have to give the prototype here */
+#include "jabs.h"
 
 int func_f(const gsl_vector *x, void *params, gsl_vector * f)
 {
