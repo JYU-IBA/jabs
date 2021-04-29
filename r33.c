b96a4d2690c88fc9036968e49806521e0ab9a48e
Jaakko Julin <jaakko.julin@jyu.fi>
2021-04-29 15:55:35 +0300

    Some preliminary code for R33 parsing.

--- r33.c
+++ r33.c
@@ -11,5 +11,50 @@
     See LICENSE.txt for the full license.
 
  */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "r33.h"
+
+r33_file *r33_alloc() {
+    r33_file *rfile = malloc(sizeof(r33_file));
+    memset(rfile, 0, sizeof(r33_file));
+    return rfile;
+}
+
+r33_file *r33_read(const char *filename) {
+    FILE *f = fopen(filename, "r");
+    if(!f)
+        return NULL;
+    r33_file *rfile = r33_alloc();
+    char *line = NULL;
+    size_t line_size = 0;
+    size_t lineno = 0;
+    while(getline(&line, &line_size, f) > 0) {
+        lineno++;
+        line[strcspn(line, "\r\n")] = 0; /* Strips all kinds of newlines! */
+        char *line_split = line;
+#if 0
+        while((col = strsep(&line_split, " ,;:")) != NULL) {
+
+        }
+#endif
+    }
+    return rfile;
+}
+
+reaction *r33_file_to_reaction(const jibal_isotope *isotopes, const r33_file *rfile) {
+    reaction *r = malloc(sizeof(reaction));
+    r->cs = JIBAL_CS_NONE;
+    r->incident = jibal_isotope_find(isotopes, NULL, rfile->zeds[0], (int)(floor(rfile->masses[0])));
+    r->target = jibal_isotope_find(isotopes, NULL, rfile->zeds[1], (int)(floor(rfile->masses[1])));
+    r->product = jibal_isotope_find(isotopes, NULL, rfile->zeds[2], (int)(floor(rfile->masses[2])));
+    r->product_nucleus = jibal_isotope_find(isotopes, NULL, rfile->zeds[3], (int)(floor(rfile->masses[3])));
+    if(rfile->filename) {
+        r->filename = strdup(rfile->filename);
+    } else {
+        r->filename = NULL;
+    }
+    /* TODO: actual data */
+    return r;
+};
--- r33.h
+++ r33.h
@@ -15,7 +15,129 @@
 #ifndef JABS_R33_H
 #define JABS_R33_H
 
+#include <jibal_masses.h>
+#include "reaction.h"
 
+#define R33_N_ADDRESS_FIELDS 9 /* Note that if this is ever changed, changes are required elsewhere too. */
+#define R33_N_QVALUES 5 /* Up to 5 Q-values are allowed */
+#define R33_N_NUCLEI 4 /* Four nuclei are involved in a reaction. No exceptions! */
+#define R33_N_SIGFACTORS 2
+#define R33_UNKNOWN "Unknown"
+
+typedef enum {
+    R33_VERSION_NONE = 0,
+    R33_VERSION_R33 = 1,
+    R33_VERSION_R33a = 2
+} r33_version;
+
+typedef enum {
+    R33_HEADER_NONE = 0,
+    R33_HEADER_COMMENT = 1,
+    R33_HEADER_VERSION = 2,
+    R33_HEADER_SOURCE = 3,
+    R33_HEADER_NAME = 4,
+    R33_ADDRESS1 = 5,
+    R33_ADDRESS2 = 6,
+    R33_ADDRESS3 = 7,
+    R33_ADDRESS4 = 8,
+    R33_ADDRESS5 = 9,
+    R33_ADDRESS6 = 10,
+    R33_ADDRESS7 = 11,
+    R33_ADDRESS8 = 12,
+    R33_ADDRESS9 = 13,
+    R33_SERIAL = 14,
+    R33_REACTION = 15,
+    R33_MASSES = 16,
+    R33_ZEDS = 17,
+    R33_COMPOSITION = 18,
+    R33_QVALUE = 19,
+    R33_DISTRIBUTION = 20,
+    R33_THETA = 21,
+    R33_ENERGY = 22,
+    R33_SIGFACTORS = 23,
+    R33_UNITS = 24,
+    R33_ENFACTORS = 25,
+    R33_NVALUES = 26,
+    R33_DATA = 27,
+    R33_ENDDATA = 28
+} r33_header_type;
+
+typedef enum {
+    R33_HEADER_OPTIONAL = 0,
+    R33_HEADER_REQUIRED = 1,
+    R33_HEADER_MUTEX1   = 2, /* Mutually exclusive: "Theta" and "Energy". One is required, if both are present we determine which one to use (based on "Distribution") */
+    R33_HEADER_MUTEX2   = 3 /* Mutually exclusive: "Nvalues" and "Data". */
+} r33_optional;
+
+typedef enum {
+    R33_UNIT_MB = 0,
+    R33_UNIT_RR = 1,
+    R33_UNIT_TOT = 2
+} r33_unit;
+
+typedef enum {
+    R33_DIST_ENERGY = 0,
+    R33_UNIT_ANGLE = 1
+} r33_distribution;
+
+typedef struct {
+    r33_header_type val;
+    const char *s;
+    r33_optional opt;
+} r33_header;
+
+static const r33_header r33_headers[] = {
+         {R33_HEADER_COMMENT,   "Comment", R33_HEADER_REQUIRED},
+         {R33_HEADER_VERSION,   "Version", R33_HEADER_OPTIONAL}, /* Required for R33a */
+         {R33_HEADER_SOURCE,    "Source", R33_HEADER_REQUIRED},
+         {R33_HEADER_NAME,      "Name", R33_HEADER_REQUIRED}, /* Author or institution. */
+         {R33_ADDRESS1,         "Address1", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS2,         "Address2", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS3,         "Address3", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS4,         "Address4", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS5,         "Address5", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS6,         "Address6", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS7,         "Address7", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS8,         "Address8", R33_HEADER_OPTIONAL},
+         {R33_ADDRESS9,         "Address9", R33_HEADER_OPTIONAL},
+         {R33_SERIAL,           "Serial Number", R33_HEADER_REQUIRED},
+         {R33_REACTION,         "Reaction", R33_HEADER_REQUIRED},
+         {R33_MASSES,           "Masses", R33_HEADER_REQUIRED},
+         {R33_ZEDS,             "Zeds", R33_HEADER_REQUIRED},
+         {R33_COMPOSITION,      "Composition", R33_HEADER_OPTIONAL},
+         {R33_QVALUE,           "Qvalue", R33_HEADER_REQUIRED},
+         {R33_DISTRIBUTION,     "Distribution", R33_HEADER_REQUIRED},
+         {R33_THETA,            "Theta", R33_HEADER_MUTEX1},
+         {R33_ENERGY,           "Energy", R33_HEADER_MUTEX1},
+         {R33_SIGFACTORS,       "Sigfactors", R33_HEADER_OPTIONAL},
+         {R33_UNITS,            "Units", R33_HEADER_OPTIONAL},
+         {R33_ENFACTORS,        "Enfactors"},
+         {R33_NVALUES,          "Nvalues", R33_HEADER_MUTEX2},
+         {R33_DATA,             "Data", R33_HEADER_MUTEX2},
+         {R33_ENDDATA,          "Enddata"},
+         {R33_HEADER_NONE, 0}
+};
+
+typedef struct r33_file {
+    char *filename;
+    char *comment;
+    r33_version version;
+    char *source;
+    char *name;
+    char *address[R33_N_ADDRESS_FIELDS];
+    int serial;
+    char *reaction; /* Reaction string, will not be parsed, but it is read */
+    double masses[R33_N_NUCLEI]; /* Reaction masses m2(m1,m3)m4 */
+    int zeds[R33_N_NUCLEI]; /* Same order as masses. */
+    char *composition;
+    double Qvalues[R33_N_QVALUES];
+    r33_distribution distribution;
+    double theta;
+    double energy;
+    double sigfactors[R33_N_SIGFACTORS];
+} r33_file;
+
+reaction *r33_file_to_reaction(const jibal_isotope *isotopes, const r33_file *rfile);
 
 
 
