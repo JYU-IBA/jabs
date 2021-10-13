ebb72da0bfca10d63d93a2b331ed3c32001f2a07
Jaakko Julin <jaakko.julin@jyu.fi>
2021-10-13 15:54:25 +0300

    Deploy script for macOS, gitignore update.

---
+++ deploy_mac.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+build_dir="build-qjabs-Desktop_x86_darwin_generic_mach_o_64bit-Release"
+jibal_install_prefix="/usr/local"
+cd "$build_dir"
+make
+echo "datadir = .
+masses_file = masses.dat
+abundances_file = abundances.dat
+files_file = files.txt
+assignments_file = assignments.txt
+" > "qjabs.app/Contents/Resources/jibal.conf"
+cp "$jibal_install_prefix/share/jibal/masses.dat" "$jibal_install_prefix/share/jibal/abundances.dat" "qjabs.app/Contents/Resources/"
+
+echo "srim2013,srim2013.ele
+yang,yang.stg
+chu,chu.stg
+bohr,bohr.stg
+" > "qjabs.app/Contents/Resources/files.txt"
+cp "$HOME/.jibal/srim2013.ele" "$HOME/.jibal/yang.stg" "$HOME/.jibal/chu.stg" "$HOME/.jibal/bohr.stg" "qjabs.app/Contents/Resources/"
+mv qjabs.app QJaBS.app
+macdeployqt QJaBS.app -dmg
+rm -rf QJaBS.app
+mv QJaBS.dmg ../
+cd ..
