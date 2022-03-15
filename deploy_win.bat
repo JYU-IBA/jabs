653ac613af36b8394eead5dc0ada5a0b9ad06cf5
Jaakko Julin <jaakko.julin@jyu.fi>
2022-03-15 13:02:20 +0200

    Renamed and updated Windows build and deploy script.

--- deploy_win.bat
+++ deploy_win.bat
@@ -1,17 +1,24 @@
+REM This script will make a standalone JaBS installation in qjabs\build\Release
+REM Install JIBAL first and run the bootstrap tool
 set JIBAL_DIR=C:\Program Files\jibal
+REM Qt6 must be installed too
+set QT6_DIR=C:\Qt\6.1.2"
+REM And vcpkg
+set VCPKG_DIR=C:\vcpkg
 mkdir build
 cd build
 del CMakeCache.txt
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:\vcpkg\scripts\buildsystems\vcpkg.cmake" ../
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" ../
 cmake --build . --target ALL_BUILD --config Release
 cd ..
 cd qjabs
 mkdir build
 cd build
 del CMakeCache.txt
-cmake -DCMAKE_PREFIX_PATH="C:\Qt\6.1.2\msvc2019_64\lib\cmake" -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:\vcpkg\scripts\buildsystems\vcpkg.cmake" ../
+cmake -DCMAKE_PREFIX_PATH="%QT6_DIR%\msvc2019_64\lib\cmake" -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake" ../
 cmake --build . --target ALL_BUILD --config Release
-C:\Qt\6.1.2\msvc2019_64\bin\windeployqt.exe Release
+REM Windeployqt will handle most dlls and other Qt dependencies
+%QT6_DIR%\msvc2019_64\bin\windeployqt.exe Release
 copy ..\..\build\Release\jabs.exe Release
 copy "%JIBAL_DIR%\bin\*.dll" Release
 del Release\jibal.conf
@@ -35,3 +42,5 @@ copy "..\..\example\example.jbs" Release\example
 copy "..\..\example\detector.jbs" Release\example
 mkdir Release\example\tests
 copy ..\..\example\tests\*.jbs Release\example\tests\
+copy ..\..\example\tests\*_ref.csv Release\example\tests\
+copy ..\..\example\tests\*.r33 Release\example\tests\
