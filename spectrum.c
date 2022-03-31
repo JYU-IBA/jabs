d34a146d94201d0cd7a74cc77ebc49a03608f884
Jaakko Julin <jaakko.julin@jyu.fi>
2022-03-31 15:33:44 +0300

    Copyright notice updates.

--- spectrum.c
+++ spectrum.c
@@ -1,7 +1,7 @@
 /*
 
     Jaakko's Backscattering Simulator (JaBS)
-    Copyright (C) 2021 Jaakko Julin
+    Copyright (C) 2021 - 2022 Jaakko Julin
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
