5011ec81c7a0d3a7fd33056788bbbd10f11e1053
Jaakko Julin <jaakko.julin@jyu.fi>
2023-01-04 16:04:20 +0200

    Copyright year update on some files.

--- src/roughness.c
+++ src/roughness.c
@@ -1,3 +1,17 @@
+/*
+
+    Jaakko's Backscattering Simulator (JaBS)
+    Copyright (C) 2021 - 2023 Jaakko Julin
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    See LICENSE.txt for the full license.
+
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <jibal_units.h>
