44b5d55f0c3a665c8be703d394932c0aa2a513a3
Jaakko Julin <jaakko.julin@jyu.fi>
2023-01-13 09:56:53 +0200

    Added plugin test (not run by run_tests).

--- example/tests/run_tests.sh
+++ example/tests/run_tests.sh
@@ -2,7 +2,9 @@
 for jabsfile in *.jbs; do
     echo "Running JaBS script $jabsfile";
     if [ "$jabsfile" == "ds.jbs" ]; then
-        echo "Skipping test."; # Takes too long...
+        echo "Skipping DS test."; # Takes too long...
+    elif [ "$jabsfile" == "plugin_cs.jbs" ]; then
+        echo "Skipping plugin test."; # JaBS can be compiled without plugin support 
     else
         jabs "$jabsfile"|| exit 1;
     fi
