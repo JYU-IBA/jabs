076f6b2e01655e7471d3df86ff907729059067e8
Jaakko Julin <jaakko.julin@jyu.fi>
2024-06-12 17:06:27 +0300

    Fixed get_qcustomplot.sh script. Might actually do what is it supposed to do now.

--- get_qcustomplot.sh
+++ get_qcustomplot.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-url="https://www.qcustomplot.com/release/2.1.1/QCustomPlot.tar.gz";
-echo "Downloading qcustomplot using curl from $url"
-curl "https://www.qcustomplot.com/release/2.1.1/QCustomPlot.tar.gz" -o QCustomPlot.tar.gz
-if echo "$(cat QCustomPlot.tar.gz.sha256)  QCustomPlot.tar.gz" | shasum -a 256 --check --status; then
-    echo "SHA sum check OK, copying qcustomplot.cpp and .h files to qjabs directory."
-    cp qcustomplot/qcustomplot.h qcustomplot/qcustomplot.cpp qjabs/
-else 
-    echo "QCustomPlot.tar.gz fails SHA sum check (see QCustomPlot.tar.gz.sha256), install shasum if not installed or verify some other way that the downloaded file is legitimate."
-fi
+qcustomplot_version="2.1.1"
+release_url="https://www.qcustomplot.com/release/${qcustomplot_version}/";
+for file in QCustomPlot.tar.gz; do
+    shafile="${file}.sha256"
+    url="${release_url}${file}"
+    echo "Downloading $file using curl from $url"
+    curl "$url" -o "$file"
+    if echo "$(cat $shafile)  QCustomPlot.tar.gz" | shasum -a 256 --check --status; then
+        echo "$file passes SHA sum check."
+        if [[ "$file" == "QCustomPlot.tar.gz" ]]; then
+            tar -xf "$file"
+            echo "Copying qcustomplot.cpp and .h files to qjabs directory."
+            cp qcustomplot/qcustomplot.h qcustomplot/qcustomplot.cpp qjabs/
+            rm -rf qcustomplot
+        fi
+    else 
+        echo "$file fails SHA sum check (see $shafile), install shasum if not installed or verify some other way that the downloaded file is legitimate."
+    fi
+done
