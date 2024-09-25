46f80253a17e77477e23365ba5dc87fc12652874
Jaakko Julin <jaakko.julin@jyu.fi>
2024-09-25 13:24:27 +0300

    Trying macOS deploy script.

--- release_scripts/deploy_mac.sh
+++ release_scripts/deploy_mac.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
-cd ..
-build_dir="build-qjabs"
-jibal_install_prefix="/usr/local"
+cd .. #Everything relative to one directory up
+#This script assumes you have installed JIBAL in ("install" directory)
+install_dir="$(pwd)/install/"
+jibal_data_file="jibal_data.tar.gz"
+jibal_data_dir="$(pwd)/jibal_data/"
+echo "Assuming JIBAL and QJaBS have been installed to $install_dir."
+if [ ! -d "${install_dir}" ]; then
+    echo "Install dir $install_dir} does not exist."
+    exit 1;
+fi
+
+if [ -f "${jibal_data_file}" ]; then
+    echo "JIBAL data file exists."
+else
+    echo "Downloading JIBAL data file."
+    curl "http://users.jyu.fi/~jaakjuli/jibal/data/data.tar.gz" -o "jibal_data.tar.gz"
+fi
+if echo "799a74a37cfd3d003a8b4914e6ff2c554c1b78eacbba84e3a6a414f982ce0bce  jibal_data.tar.gz" | shasum -a 256 --check --status; then
+        echo "$jibal_data_file passes SHA sum check."
+    else
+        echo "$jibal_data_file does not pass SHA sum check."
+        shasum -a 256 "$jibal_data_file"
+        exit 1;
+fi
+
+rm -rf jibal_data
+mkdir -p jibal_data
+tar zxvf jibal_data.tar.gz -C "${jibal_data_dir}"
+
 read version < version.txt
 arch=$(uname -m)
-rm -f example.zip
-mkdir -p "$build_dir"
-cmake -S qjabs -B "$build_dir"
-cd "$build_dir" || exit 1
-make
+
 echo "datadir = .
 masses_file = masses.dat
 abundances_file = abundances.dat
 files_file = files.txt
 assignments_file = assignments.txt
 " > "qjabs.app/Contents/Resources/jibal.conf"
-cp "$jibal_install_prefix/share/jibal/masses.dat" "$jibal_install_prefix/share/jibal/abundances.dat" "qjabs.app/Contents/Resources/"
+cp "${install_dir}/share/jibal/masses.dat" "${install_dir}/share/jibal/abundances.dat" "${install_dir}/qjabs.app/Contents/Resources/"
 
 echo "srim2013,srim2013.ele
 yang,yang.stg
 chu,chu.stg
 bohr,bohr.stg
 " > "qjabs.app/Contents/Resources/files.txt"
-cp "$HOME/.jibal/srim2013.ele" "$HOME/.jibal/yang.stg" "$HOME/.jibal/chu.stg" "$HOME/.jibal/bohr.stg" "qjabs.app/Contents/Resources/"
-rm -rf JaBS.app
+cp "$jibal_data_dir"/*.ele "${install_dir}/qjabs.app/Contents/Resources/"
+cp "$jibal_data_dir"/*.stg "${install_dir}/qjabs.app/Contents/Resources/"
+cd "$install_dir"
 mv qjabs.app JaBS.app
 macdeployqt JaBS.app -dmg
-#rm -rf JaBS.app
-mv JaBS.dmg "../JaBS $version macOS $arch.dmg"
-cd ..
-find example|grep -v -e "/\." -e "_out\."|zip example -@
+mv JaBS.dmg "JaBS $version macOS $arch.dmg"
