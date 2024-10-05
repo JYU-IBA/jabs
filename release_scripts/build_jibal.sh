b5c39754f810588a8d8e392fef2a9869ce05258d
Jaakko Julin <jaakko.julin@jyu.fi>
2024-10-05 19:32:58 +0300

    Build script changes to build something that works on macOS. Github build is untested.

---
+++ release_scripts/build_jibal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ..
+jibal_src_dir="../jibal"
+builddir="build_jibal"
+if [ ! -d "$jibal_src_dir" ]; then
+    echo "Tried to find JIBAL source code from directory $jibal_src_dir (relative to $(pwd))"
+    echo "Perhaps run this in the parent directory (of $(pwd)): "
+    echo "git clone https://github.com/JYU-IBA/jibal.git"
+    exit 1
+fi
+mkdir -p "$builddir";
+if ! cmake -S "$jibal_src_dir" -B "$builddir" -DCMAKE_BUILD_TYPE=Release then
+    echo "Could not configure using CMake"
+    exit 1;
+fi
+
+if ! cmake --build "$builddir" --config Release; then
+    echo "Could not build."
+    exit 1;
+fi
+
+if ! sudo cmake --install "$builddir" then
+    echo "Could not install."
+    exit 1;
+fi
--- release_scripts/deploy_mac.sh
+++ release_scripts/deploy_mac.sh
@@ -1,54 +1,77 @@
 #!/bin/bash
 cd .. #Everything relative to one directory up
-#This script assumes you have installed JIBAL in ("install" directory)
-install_dir="$(pwd)/install/"
-jibal_data_file="jibal_data.tar.gz"
-jibal_data_dir="$(pwd)/jibal_data/"
-echo "Assuming JIBAL and QJaBS have been installed to $install_dir."
-if [ ! -d "${install_dir}" ]; then
-    echo "Install dir $install_dir} does not exist."
+#This script assumes you have installed JIBAL somewhere (so that CMake can find it and jibaltool can be run)
+#Try running build_jibal.sh and build_qjabs.sh first
+builddir="build_qjabs"
+if ! jibaltool status; then echo
+    "Install JIBAL first. CMake should be able to find it and I need to run jibaltool."
+    "Check or run build_jibal.sh"
+fi
+jibal_datadir="$(jibaltool config 2>&1|grep "^datadir = "|sed "s/^datadir = //")"
+jibal_gsto_data_file="jibal_data.tar.gz"
+jibal_gsto_data_dir="$(pwd)/jibal_data/"
+echo "Assuming JIBAL data (masses.dat etc) is found in $jibal_datadir."
+
+echo "Assuming QJaBS is built in $builddir"
+if [ ! -d "${builddir}" ]; then
+    echo "Build directory ${builddir} does not exist."
+    echo "Try running build_qjabs.sh"
     exit 1;
 fi
 
-export PATH="$PATH:${install_dir}/bin"
 
-if [ -f "${jibal_data_file}" ]; then
+read version < version.txt
+arch=$(uname -m)
+echo "Arch $arch"
+output="JaBS $version macOS $arch.dmg"
+
+if [ -f "${jibal_gsto_data_file}" ]; then
     echo "JIBAL data file exists."
 else
     echo "Downloading JIBAL data file."
     curl "http://users.jyu.fi/~jaakjuli/jibal/data/data.tar.gz" -o "jibal_data.tar.gz"
 fi
 if echo "799a74a37cfd3d003a8b4914e6ff2c554c1b78eacbba84e3a6a414f982ce0bce  jibal_data.tar.gz" | shasum -a 256 --check --status; then
-        echo "$jibal_data_file passes SHA sum check."
+        echo "$jibal_gsto_data_file passes SHA sum check."
     else
-        echo "$jibal_data_file does not pass SHA sum check."
-        shasum -a 256 "$jibal_data_file"
+        echo "$jibal_gsto_data_file does not pass SHA sum check."
+        shasum -a 256 "$jibal_gsto_data_file"
         exit 1;
 fi
 
 rm -rf jibal_data
+rm -f "$output"
 mkdir -p jibal_data
-tar zxvf jibal_data.tar.gz -C "${jibal_data_dir}"
+tar zxvf jibal_data.tar.gz -C "${jibal_gsto_data_dir}"
 
-read version < version.txt
-arch=$(uname -m)
-echo "Arch $arch"
 echo "datadir = .
 masses_file = masses.dat
 abundances_file = abundances.dat
 files_file = files.txt
 assignments_file = assignments.txt
-" > "${install_dir}/qjabs.app/Contents/Resources/jibal.conf"
-cp "${install_dir}/share/jibal/masses.dat" "${install_dir}/share/jibal/abundances.dat" "${install_dir}/qjabs.app/Contents/Resources/"
+" > "${builddir}/qjabs.app/Contents/Resources/jibal.conf"
+cp "${jibal_datadir}/masses.dat" "${jibal_datadir}/abundances.dat" "${builddir}/qjabs.app/Contents/Resources/"
 echo "srim2013,srim2013.ele
 yang,yang.stg
 chu,chu.stg
 bohr,bohr.stg
-" > "${install_dir}/qjabs.app/Contents/Resources/files.txt"
-cp "$jibal_data_dir"/*.ele "${install_dir}/qjabs.app/Contents/Resources/"
-cp "$jibal_data_dir"/*.stg "${install_dir}/qjabs.app/Contents/Resources/"
-cd "$install_dir"
-mv qjabs.app JaBS.app
+" > "${builddir}/qjabs.app/Contents/Resources/files.txt"
+cp "$jibal_gsto_data_dir"/*.ele "${builddir}/qjabs.app/Contents/Resources/"
+cp "$jibal_gsto_data_dir"/*.stg "${builddir}/qjabs.app/Contents/Resources/"
+cd "$builddir"
+echo "Operating in $pwd"
+#mv qjabs.app JaBS.app
+rm -rf JaBS.app
+cp -R qjabs.app JaBS.app
 echo "Running macdeployqt"
-macdeployqt6 JaBS.app -dmg -no-strip -libpath="${install_dir}/lib"
-mv JaBS.dmg "JaBS $version macOS $arch.dmg"
+if [ -z "${JABS_SIGN_IDENTITY}" ]; then 
+    codesign_opt="";
+else
+    echo "Signing with $JABS_SIGN_IDENTITY"
+    codesign_opt=-codesign="${JABS_SIGN_IDENTITY}";
+fi
+
+#cp lib/libjibal.0.dylib JaBS.app/Contents/Frameworks/
+#install_name_tool -change /opt/homebrew/opt/gsl/lib/ "@executable_path/../Frameworks/" JaBS.app/Contents/Frameworks/libjibal.0.dylib
+macdeployqt6 JaBS.app -dmg -no-strip ${codesign_opt}
+mv JaBS.dmg "$output"
