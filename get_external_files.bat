4b75bacf5e653975c7fae26329578f7026694516
Jaakko Julin <jaakko.julin@jyu.fi>
2024-10-06 19:53:54 +0300

    Can't even copy?

--- get_external_files.bat
+++ get_external_files.bat
@@ -1,4 +1,4 @@
 curl "https://www.qcustomplot.com/release/2.1.1/QCustomPlot.tar.gz" -o QCustomPlot.tar.gz
 tar -zxvf QCustomPlot.tar.gz
-copy "qcustomplot/qcustomplot.h" qjabs
-copy "qcustomplot/qcustomplot.cpp" qjabs
+copy qcustomplot\qcustomplot.h qjabs
+copy qcustomplot\qcustomplot.cpp qjabs
